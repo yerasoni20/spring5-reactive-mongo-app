4476b4ec26169b0fd051ea55ec1b88e81d3f9ed0
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-28 16:22:13 -0400

    Project for Reactive programming, MongoDB, recipe app

--- src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
@@ -4,9 +4,6 @@ import guru.springframework.domain.Category;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-/**
- * Created by jt on 8/17/17.
- */
 public interface CategoryReactiveRepository extends ReactiveMongoRepository<Category, String> {
 
     Mono<Category> findByDescription(String description);
