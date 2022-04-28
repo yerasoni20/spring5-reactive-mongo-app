c2cb22753bd8daff1254aeccdcecc4072ee92f9b
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-28 16:21:37 -0400

    Project for Reactive programming, MongoDB, recipe app

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -6,9 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-/**
- * Created by jt on 6/13/17.
- */
+
 @Getter
 @Setter
 @Document
