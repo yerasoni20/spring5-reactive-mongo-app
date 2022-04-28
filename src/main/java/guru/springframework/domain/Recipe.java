7d0a044c950ee365b8bdb50dae2073f39f190309
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-28 16:21:23 -0400

    Project for Reactive programming, MongoDB, recipe app

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -8,9 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by jt on 6/13/17.
- */
+ 
 @Getter
 @Setter
 @Document
