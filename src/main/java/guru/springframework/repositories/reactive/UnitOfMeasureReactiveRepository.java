1702edf65572be27e5b44571f6a184ec4b38a1c0
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-04-28 16:22:43 -0400

    Update UnitOfMeasureReactiveRepository.java

--- src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -4,9 +4,7 @@ import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-/**
- * Created by jt on 8/17/17.
- */
+
 public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
 
     Mono<UnitOfMeasure> findByDescription(String description);
